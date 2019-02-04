f06b8c2235034083d1bd600cf27d50aaa31a4ea2
Chris Griffith <chris.griffith@gmail.com>
2019-02-04 13:08:04 -0800

    Demos Updated
    Action Sheet
    Alert
    Badge

--- action-sheet/src/app/home/home.page.ts
+++ action-sheet/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  constructor(
+    private actionSheetController: ActionSheetController,
+    private platform: Platform
+  ) {
+    this.presentActionSheet();
+  }
+
+  displayActionSheet() {
+    this.presentActionSheet();
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Manage Email',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: !this.platform.is('ios') ? 'trash' : null,
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Forward To',
+        icon: !this.platform.is('ios') ? 'share-alt' : null,
+        handler: () => {
+          console.log('Forward clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: !this.platform.is('ios') ? 'star' : null,
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Send',
+        icon: !this.platform.is('ios') ? 'send' : null,
+        handler: () => {
+          console.log('Send clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: !this.platform.is('ios') ? 'close' : null,
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
 }
--- alert/src/app/home/home.page.ts
+++ alert/src/app/home/home.page.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
+  constructor(
+    private alertController: AlertController
+  ) {}
+
+  ngOnInit() { }
+
+  displayAlert() {
+    // this.presentAlert();
+    // this.presentAlertConfirm();
+    this.presentAlertPrompt();
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert Header',
+      subHeader: 'Subheader',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Alert Header',
+      message: 'This is an alert message.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+         handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      header: 'Enter Username',
+      inputs: [
+        {
+          name: 'username',
+          type: 'text',
+          placeholder: 'name@company.com'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: ( data ) => {
+            console.log('Confirm Ok', data.username);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
