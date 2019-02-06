70e4e6b1fe5c9cac54783c2a1432c99c532f46bb
Chris Griffith <chris.griffith@gmail.com>
2019-02-05 20:00:03 -0800

    Ionic Spinner Demo

--- spinner/src/app/home/home.page.ts
+++ spinner/src/app/home/home.page.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public isMapLoaded: Boolean = false;
 
+  constructor() { }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.isMapLoaded = true;
+    }, 5000);
+  }
 }
